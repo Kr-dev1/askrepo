c43e9e8d50c63c793435be74715dacc7bc3cd60d
kasturirangan <kasturirangan20@gmail.com>
2025-05-08 23:58:16 +0530

    test

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,20 @@
+import { NextConfig } from "next";
+import { Configuration as WebpackConfig, WebpackPluginInstance } from "webpack";
 import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config: any, { isServer }) => {
+const nextConfig: NextConfig = {
+  webpack: (config: WebpackConfig, { isServer }: { isServer: boolean }) => {
     if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
+      // Make sure plugins exists before modifying it
+      if (!config.plugins) {
+        config.plugins = [];
+      }
+
+      // Explicitly cast PrismaPlugin as WebpackPluginInstance
+      config.plugins.push(
+        new PrismaPlugin() as unknown as WebpackPluginInstance
+      );
     }
     return config;
   },
